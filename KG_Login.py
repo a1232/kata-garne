86c377e8c452a98df6e9dfc4d3e3731f48f60e12
a1232 <45252766+a1232@users.noreply.github.com>
2024-03-04 17:25:08 +0545

    KG_Login.py

--- KG_Login.py
+++ KG_Login.py
@@ -5,7 +5,10 @@ import sqlite3
 def verify():
     email = Entry1.get()
     password = Entry2.get()
-    try:
+    if email=="" or password==" ":
+        messagebox.showerror("Warning!","All field must be filled")
+    else:
+     try:
         conI = sqlite3.connect("DataEntry.db")
         curI = conI.cursor()
         curI.execute("CREATE TABLE IF NOT EXISTS data(email TEXT, password TEXT)")
@@ -16,16 +19,18 @@ def verify():
             if stored_password and stored_password[0] == password:
                 messagebox.showinfo("Verification", "Login successful!")
                 Kg.destroy()
-                import new
+                import Venue
             else:
                 messagebox.showerror("Verification", "Invalid Password")
         else:
             messagebox.showerror("Verification", "Invalid email")
         conI.commit()
         conI.close()
-    except Exception as e:
+     except Exception as e:
         messagebox.showerror("Verification", f"Error: {e}")
-
+def prev():
+    Kg.destroy()
+    import portal
 Kg= Tk()
 Kg.title("Kata Garne? Login")
 Backgrnd=PhotoImage(file='KGPR6.png')
@@ -33,16 +38,14 @@ Backgrnd1=Label(Kg,image=Backgrnd).place(x=0,y=0)
 Kg.maxsize(width=1240, height=720)
 Kg.minsize(width=1240,height=720)
 
-
-
 Customer_mail= Label(Kg, text="E-mail:",font=('helvetica',18,'bold'),bg='#f8f5ee')
-Customer_mail.place(x=800,y=200)
+Customer_mail.place(x=800,y=230)
 Entry1=Entry(Kg,width=25,font=("helvetica",12,'italic'))
-Entry1.place(x=910,y=208)
+Entry1.place(x=910,y=238)
 Customer_password= Label(Kg,text="Password:",font   =('helvetica',18,'bold'),bg='#f8f5ee')
-Customer_password.place(x=780,y=250)
+Customer_password.place(x=780,y=290)
 Entry2= Entry(Kg,width=25,font=("helvetica",12,'italic'),show="*")
-Entry2.place(x=910,y=258)
+Entry2.place(x=910,y=298)
 
 def pas():
     if pH.get()==1:
@@ -51,13 +54,10 @@ def pas():
         Entry2.config(show="*")
 pH=IntVar()
 showPH=Checkbutton(Kg,text="Show Password",width=15, font=("helvetica", 12, 'italic'),command=pas, variable=pH, onvalue=1 ,bg="#f8f5ee")
-showPH.place(x=930,y=300)
+showPH.place(x=930,y=370)
 
-Update=Button(Kg,text="Signin",width=25,font=('helvetica',10,'bold'),command=verify).place(x=880,y=350)
-Seperate=Label(Kg,text="OR \n LOGIN USING:",font=('helvetica',10,'bold'),bg='#f8f5ee').place(x=940,y=400)
-Fb=Button(Kg,text="Facebook",width=25,bg="Blue",fg="white",font=('helvetica',11,'bold')).place(x=870,y=460)
-Gl=Button(Kg,text="Google+",width=25,bg="Red",fg='white',font=('helvetica',11,'bold')).place(x=870,y=510)
-
-
+Update=Button(Kg,text="Signin",width=25,font=('helvetica',10,'bold'),command=verify).place(x=900,y=420)
+back_but=Button(Kg,text='<Back',font=('Helvetica',14),command=prev)
+back_but.place(x=10,y=20)
 
 Kg.mainloop()
